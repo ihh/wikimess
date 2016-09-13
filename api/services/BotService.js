7cf60154c2213d5ded2df009da94f18952936771
Ian Holmes <ihh@berkeley.edu>
2016-09-12 18:18:12 -0700

    botmind

--- api/services/BotService.js
+++ api/services/BotService.js
@@ -2,19 +2,6 @@
 
 module.exports = {
 
-    firstMove: function (player, role, choice) {
-	var role = Game.getRole (game, player.id)
-	var mood = Game.getRoleAttr (game, role, 'mood')
-	switch (player.botmind.strategy) {
-	case 'mood':
-	    return Math.random() < player.botmind.probc[mood] ? 'c' : 'd'
-	    break;
-	default:
-	    break;
-	}
-	return 'd'
-    },
-
     randomMove: function (player, game) {
 	var role = Game.getRole (game, player.id)
 	var mood = Game.getRoleAttr (game, role, 'mood')
