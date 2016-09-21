a40f688985994c5f588e2e1a102c6cbef345c036
Ian Holmes <ihh@berkeley.edu>
2016-09-20 22:02:23 -0700

    Refactoring to use sails task pipeline

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -31,6 +31,9 @@ var jsFilesToInject = [
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
 
+  // External libraries
+  'js/ext/**/*.js',
+
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
   'js/**/*.js'
