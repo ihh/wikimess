3809adf515a721927e04bc7a2322b2067f5a44ef
Ian Holmes <ihh@berkeley.edu>
2017-12-03 15:05:16 -0800

    selective subset of thesaurus

--- data/symbols/moby.js
+++ data/symbols/moby.js
@@ -1,25 +1,27 @@
 var fs = require('fs')
 var readline = require('readline')
-
 var filename = 'mobythes.aur'
+var words = ['delightful', 'rabid']
 
 if (fs.existsSync (filename)) {
   var lineReader = readline.createInterface({
     input: require('fs').createReadStream (filename)
   })
 
-  var result = []
+  var rules = {}
   lineReader.on('line', function (line) {
     var defs = line.split(',')
-    var name = defs[0]
-    var rules = defs.slice(1)
-        .filter (function (text) { return text.length })
-        .map (function (text) { return [text] })
-    result.push ({ name: name,
-                   rules: rules })
+    var name = defs[0].toLowerCase().replace(/\s\-,/g,'_')
+    rules[name] = defs.slice(1)
+      .filter (function (text) { return text.length })
+      .map (function (text) { return [text] })
   })
 
   lineReader.on('close', function() {
+    var result = words.map (function (name) {
+      return { name: name,
+               rules: rules[name] }
+    })
     console.log (JSON.stringify (result))
   })
 }
