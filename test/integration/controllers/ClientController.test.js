75f56151e9660822bb81eecc5d6367cc013d4328
Ian Holmes <ihh@berkeley.edu>
2017-12-09 20:57:30 -0800

    Added a test demonstrating #74

--- test/integration/controllers/ClientController.test.js
+++ test/integration/controllers/ClientController.test.js
@@ -1,23 +1,52 @@
-var request = require('supertest');
+var session = require('supertest-session');
+
+    var testSession = null;
+    beforeEach (function () {
+      testSession = session (sails.hooks.http.app);
+    });
+    
 
 describe('ClientController', function() {
 
-  describe('#login()', function() {
-    it('should log admin in', function (done) {
-      request(sails.hooks.http.app)
-        .post('/login')
+    it('should fail accessing a restricted page', function (done) {
+      testSession.get('/p/drafts')
+        .expect(401)
+        .end(done)
+    });
+    
+    it('should sign in', function (done) {
+      testSession.post('/login')
         .send({ name: 'admin', password: 'admin' })
         .expect(200)
         .expect('Content-Type', /json/)
         .expect(function (res) {
           if (!('player' in res.body)) throw new Error("missing player key");
           if (!('id' in res.body.player)) throw new Error("missing player.id key");
-//          if (res.body.player.id !== 1) throw new Error("player.id has wrong value");
         }).end(function(err, res) {
           if (err) return done(err);
-          done();
+          return done();
         });
     });
-  });
+
+  describe('(after authenticating session)', function() {
+
+    var authenticatedSession = null;
+    beforeEach (function (done) {
+      testSession.post('/login')
+        .send({ name: 'fred', password: 'test' })
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          authenticatedSession = testSession;
+          return done();
+        });
+    });
+    
+    it('should access a restricted page', function (done) {
+      authenticatedSession.get('/p/drafts')
+        .expect(200)
+        .end(done)
+    });
+  })
 
 });
