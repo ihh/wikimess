0a8f01d6b980f44cc4ab6e865e7e28398ac3b7cb
Ian Holmes <ihh@berkeley.edu>
2018-04-23 01:33:42 -0700

    local_example.js

---
+++ config/local_example.js
@@ -0,0 +1,19 @@
+// You need something like this for Facebook/Twitter integration
+
+/*
+module.exports.local = {
+
+// OAuth strategy configuration
+ facebook: {
+   clientID: 'YOUR_CLIENT_ID',
+   clientSecret: 'YOUR_CLIENT_SECRET',
+   enableProof: false
+ },
+
+ twitter: {
+   consumerKey: 'YOUR_CONSUMER_KEY',
+   consumerSecret: 'YOUR_CONSUMER_SECRET'
+ },
+
+};
+*/
